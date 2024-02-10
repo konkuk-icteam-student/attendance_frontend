991f5cac85d0395166d8c477cd2856583274ba7b
nagyeong kim <skrud0318@naver.com>
2024-02-10 23:34:34 +0900

    fix: Modified exception handlingcode for minute time input

--- src/util/stringUtils.js
+++ src/util/stringUtils.js
@@ -2,16 +2,32 @@ import { Minimize } from "@mui/icons-material";
 
 //화면에 띄워줄 시간 포맷 함수 PT10H10M10S -> 10:10:10
 export const formatTime = (time) => {
-  if (time == "PT0S") return "0시간 0분";
-  let hour = time.split("T")[1].split("H")[0];
-  let minute = time.split("T")[1].split("H")[1].split("M")[0];
-  if (hour.length === 1) hour = "0" + hour;
-  else if (hour.length === 0) hour = "00";
+  console.log("time", time);
 
-  if (minute.length === 1) minute = "0" + minute;
-  else if (minute.length === 0) minute = "00";
+  //시간에 H가 들어가있으면
+  if (time.includes("H")) {
+    let hour = time.split("H")[0].replace("PT", "");
+    let minute = time.split("H")[1].split("M")[0];
+    let second = time.split("M")[1].split("S")[0];
+    if (hour.length === 1) hour = "0" + hour;
+    else if (hour.length === 0) hour = "00";
 
-  return `${hour}시간 ${minute}분`;
+    if (minute.length === 1) minute = "0" + minute;
+    else if (minute.length === 0) minute = "00";
+
+    if (second.length === 1) second = "0" + second;
+    else if (second.length === 0) second = "00";
+
+    return `${hour}시간 ${minute}분`;
+  } else if (time.includes("M")) {
+    let minute = time.split("M")[0].replace("PT", "");
+    if (minute.length === 1) minute = "0" + minute;
+    else if (minute.length === 0) minute = "00";
+
+    return `${minute}분`;
+  } else {
+    return "0시간 0분";
+  }
 };
 
 //백엔드 서버로 보내줄 시간 포맷 함수 2024-01-24T00:15:00.000 -> 2024-01-24 00:15:00.000
