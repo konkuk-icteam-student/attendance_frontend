a819b5aee50b2fba7b7569c7d3266f18b3dda073
Nagyeong3 <skrud0318@naver.com>
2024-02-21 19:43:47 +0900

    feat: Added input field for department headcount"

--- src/pages/admin/DepartmentManagementPage.js
+++ src/pages/admin/DepartmentManagementPage.js
@@ -8,21 +8,34 @@ function DepartmentManagementPage() {
   const [error, setError] = useState(null);
   const [deptOID, setDeptOID] = useState();
   const [deptList, setDeptList] = useState([]);
+  const [workerNum, setWorkerNum] = useState(0);
 
   const StoredDeptID = window.localStorage.getItem("deptID");
   const handleDeptChange = (event) => {
     setDeptOID(event.target.value);
   };
+  const handleChange = (event) => {
+    setWorkerNum(event.target.value);
+  };
+
   const handleAddDept = async () => {
     try {
-      const response = await client.post("/dept/new-dept", {
-        deptName: department,
-        workerNum: 23,
-      });
-      console.log(response);
-      setDepartment("");
-
-      alert("부서 추가 완료");
+      const response = await client
+        .post("/dept/new-dept", {
+          deptName: department,
+          workerNum: parseInt(workerNum, 10), // Convert to integer
+        })
+        .then((res) => {
+          setDepartment("");
+          alert("부서 추가 완료");
+          setLoading(true);
+          setWorkerNum(0);
+        })
+        .catch((e) => {
+          alert("부서명이 중복됩니다.");
+          setDepartment("");
+          setWorkerNum(0);
+        });
     } catch (e) {
       setError(e);
     }
@@ -50,7 +63,7 @@ function DepartmentManagementPage() {
   return (
     <div>
       <div>
-        <h2>부서 추가 page</h2>
+        <h4> &nbsp; 부서 관리 페이지</h4>
       </div>
       <br />
       <InputGroup className="mb-3 w-50 mx-auto">
@@ -58,22 +71,37 @@ function DepartmentManagementPage() {
         <input
           className="form-control form-control-sm"
           type="text"
-          placeholder="부서명"
+          placeholder="추가할 부서 명"
           aria-label=".form-control-sm example"
           value={department}
           onChange={(e) => {
             setDepartment(e.target.value);
           }}
         />
+        <InputGroup.Text id="basic-addon2">인원수</InputGroup.Text>
+        <input
+          className="form-control form-control-sm"
+          type="number"
+          placeholder="인원수"
+          aria-label=".form-control-sm example"
+          value={workerNum}
+          onChange={handleChange}
+        />
       </InputGroup>
-      <div>
-        &nbsp; 텍스트 필드 대신 건국대 모든 부서 목록 띄워두는게 좋을 듯함.
+
+      <div className="row justify-content-end mt-3">
+        <button
+          className="btn btn-outline-secondary"
+          type="button"
+          style={{ width: "10%", marginRight: "25%" }}
+          onClick={handleAddDept}
+        >
+          부서 추가
+        </button>
       </div>
-      <button className="mx-auto" onClick={handleAddDept}>
-        부서 추가버튼{" "}
-      </button>
-      <div className="col">
-        {" "}
+      <br />
+      <InputGroup className="mb-3 w-50 mx-auto">
+        <InputGroup.Text id="basic-addon1">부서</InputGroup.Text>
         <select
           onChange={handleDeptChange}
           className="form-select"
