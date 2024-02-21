f3de12cee62b553a11327f40ddc1ed9aec2ea88d
Nagyeong3 <skrud0318@naver.com>
2024-02-21 19:19:16 +0900

    feat: Add functionality about list of working students

--- src/component/Board.js
+++ src/component/Board.js
@@ -1,55 +1,63 @@
-import React, { useEffect, useMemo, useState, useRef } from "react";
+import React from "react";
+import Grid from "@mui/material/Grid";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import Divider from "@mui/material/Divider";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
-import Avatar from "@mui/material/Avatar";
-import Typography from "@mui/material/Typography";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-function Board() {
-  const workingMembers = [
-    { name: "김호준", id: "1" },
-    { name: "김나경", id: "2" },
-    { name: "박준형", id: "3" },
-    { name: "지민영", id: "4" },
-    { name: "문지영", id: "5" },
-  ];
+
+function Board(props) {
+  const { deptMembers } = props;
+
+  const numRows = Math.ceil(deptMembers.length / 2);
+  const firstColumnMembers = deptMembers.slice(0, numRows);
+  const secondColumnMembers = deptMembers.slice(numRows);
 
   return (
-    <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-      <div className="row justify-content-center">
-        {workingMembers.map((member) => {
-          return (
-            <div key={member.id}>
-              <ListItem alignItems="flex-start">
-                <ListItemAvatar>
-                  <AccountCircleIcon fontSize="large" />
-                </ListItemAvatar>
-                <ListItemText
-                  primary={member.name}
-                  secondary={
-                    <React.Fragment>
-                      <Typography
-                        sx={{ display: "inline" }}
-                        component="span"
-                        variant="body2"
-                        color="text.primary"
-                      >
-                        개발팀
-                      </Typography>
-                      <br />
-                      {" 개인 상태 메시지 ---"}
-                    </React.Fragment>
+    <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+      <Grid container spacing={2} justifyContent="center">
+        {[firstColumnMembers, secondColumnMembers].map(
+          (column, columnIndex) => (
+            <Grid
+              container
+              item
+              xs={12}
+              sm={6}
+              key={columnIndex}
+              justifyContent={columnIndex === 0 ? "flex-start" : "flex-end"}
+            >
+              {column.map((member, index) => (
+                <Grid
+                  item
+                  key={member.id}
+                  xs={12}
+                  textAlign={
+                    columnIndex === 0 && index === numRows - 1
+                      ? "left"
+                      : "center"
                   }
-                />
-              </ListItem>
-              <Divider variant="inset" component="li" />
-            </div>
-          );
-        })}
-      </div>
+                >
+                  <ListItem alignItems="flex-start">
+                    <ListItemAvatar>
+                      <AccountCircleIcon fontSize="large" />
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary={member.userName}
+                      secondary={member.userId}
+                    />
+                  </ListItem>
+                  {index < numRows - 1 && (
+                    <Divider variant="inset" component="li" />
+                  )}
+                </Grid>
+              ))}
+            </Grid>
+          )
+        )}
+      </Grid>
     </List>
   );
 }
+
 export default Board;
