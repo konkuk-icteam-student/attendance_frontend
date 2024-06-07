fde4e3e19d07e0d0cd1acc6a6c03b3cfcd491fc6
박세준 <74056843+sejoon00@users.noreply.github.com>
2024-06-07 14:14:47 +0900

    baseURL 설정

--- src/util/clients.js
+++ src/util/clients.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const client = axios.create({
   withCredentials: true, // 이 옵션은 쿠키를 포함한 요청을 허용합니다.
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 export default client;
