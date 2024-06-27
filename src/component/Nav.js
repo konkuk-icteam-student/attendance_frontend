9371108985d8381c1edfa2cac67e2ce1d6772d78
박세준 <74056843+sejoon00@users.noreply.github.com>
2024-06-27 14:44:04 +0900

    log auth

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -58,7 +58,7 @@ function MyNav({ deptName }) {
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
-                  {auth ? (
+                  {auth != null || auth != undefined ? (
                     <>
                       <Nav.Link href="#">환영합니다! {auth.name} 님</Nav.Link>
                       <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
